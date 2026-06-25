73dab8136fa97055c672a42eb20b9a56d9873dc1
TahaSrn <tahaahmadi0403@gmail.com>
2026-06-25 12:35:16 +0330

    adding pagination

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,24 +5,29 @@ import { useSearchParams } from "react-router";
 export function useBookings() {
   const [searchParams, setSearchParams] = useSearchParams();
 
+  //FILTER
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
 
+  //SORT
   const sortByRow = searchParams.get("sortBy") || "startDate-desc";
   const [field, direction] = sortByRow.split("-");
   const sortBy = { field, direction };
 
+  //PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
   const {
     isLoading,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isLoading, error, bookings };
+  return { isLoading, error, bookings, count };
 }
